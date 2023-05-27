2f5007e7e62f93f34600090a88791fde7b2a55fd
Sycri <39947226+Sycri@users.noreply.github.com>
2023-05-27 23:43:47 +0300

    Switch to Material 3

--- app/src/main/java/lv/lu/students/lk17235/datz4019/ui/main/settings/SettingsViewModel.kt
+++ app/src/main/java/lv/lu/students/lk17235/datz4019/ui/main/settings/SettingsViewModel.kt
@@ -5,8 +5,4 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SettingsViewModel : ViewModel() {
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
-    }
-    val text: LiveData<String> = _text
 }
